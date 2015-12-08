272b4ddf95a7ee5cafb662f2efb0330b7c6e7cd2
William Chargin <wchargin@gmail.com>
2015-12-08 15:42:23 -0800

    Extract App to PitchCalculator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,9 @@
 import React, {Component} from 'react';
 
-import ChordComputer from './ChordComputer';
-import RationalizerConfig from './RationalizerConfig';
-
-import {extendRationalizer, canonicalRationalizer} from '../HarmonicSeries';
-const initialInputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-const initialValues = initialInputs.map(canonicalRationalizer);
-const names = [
-    "m2", "M2", "m3", "M3",
-    "P4", "A4/d5", "P5",
-    "m6", "M6", "m7", "M7"
-];
+import PitchCalculator from './PitchCalculator';
 
 export default class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            values: initialValues,
-        };
-    }
-
     render() {
-        return <div className="container">
-            <h1>Var&egrave;se pitch calculator</h1>
-            <ChordComputer
-                rationalizer={extendRationalizer(this.state.values)}
-            />
-            <RationalizerConfig
-                values={this.state.values}
-                defaults={initialValues}
-                names={names}
-                onChangeValue={(newValue, index) => {
-                    const {values} = this.state;
-                    this.setState({
-                        values: [
-                            ...values.slice(0, index),
-                            newValue,
-                            ...values.slice(index + 1),
-                        ],
-                    });
-                }}
-            />
-        </div>;
+        return <PitchCalculator />;
     }
 }
