9afa7cbe33855f2494afead5cdbdb94887c4d706
William Chargin <wchargin@gmail.com>
2015-12-08 15:23:35 -0800

    Add a view component for complete binary trees
    Summary:
    We plan to use this to show outfolding trees of trichords.
    
    Test Plan:
    Apply the following patch, and look at the app page:
    ```
    
    diff --git a/src/components/App.jsx b/src/components/App.jsx
    index 7e2c9b4..aca24e3 100644
    --- a/src/components/App.jsx
    +++ b/src/components/App.jsx
    @@ -2,6 +2,7 @@ import React, {Component} from 'react';
    
     import ChordComputer from './ChordComputer';
     import RationalizerConfig from './RationalizerConfig';
    +import TreeView from './TreeView';
    
     import {extendRationalizer, canonicalRationalizer} from '../HarmonicSeries';
     const initialInputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
    @@ -41,6 +42,10 @@ export default class App extends Component {
                         });
                     }}
                 />
    +            <TreeView
    +                elements={[["0"], ["1", "2"], ["3", "4", "5", "6"]].map(
    +                    ss => ss.map(s => <button>{s}</button>))}
    +            />
             </div>;
         }
     }
    ```

---
+++ src/components/TreeView.jsx
@@ -0,0 +1,57 @@
+import React, {Component, PropTypes} from 'react';
+
+/*
+ * Lay out a bunch of components in a complete binary tree.
+ */
+export default class TreeView extends Component {
+
+    render() {
+        return <div>
+            {this.props.elements.map(this._renderRow.bind(this))}
+        </div>;
+    }
+
+    _renderRow(row, rowIndex) {
+        return <div key={rowIndex} style={{
+            display: "table",
+            width: "100%",
+            borderCollapse: "separate",
+            borderSpacing: this.props.spacing,
+            marginBottom: -this.props.spacing,  // counteract double-spacing
+        }}>
+            <div style={{
+                display: "table-row",
+                height: this.props.rowHeight,  // might be undefined; fine
+            }}>
+                {row.map((node, nodeIndex) =>
+                    <div key={nodeIndex} style={{
+                        display: "table-cell",
+                        textAlign: "center",
+                        verticalAlign: "middle",
+                    }}>{node}</div>)}
+            </div>
+        </div>;
+    }
+
+}
+TreeView.propTypes = {
+
+    /*
+     * A ragged array like [[1], [2, 3], [4, 5, 6, 7], [8, ..., 15]]
+     * containing the rows to render, in left-to-right order.
+     */
+    elements: PropTypes.arrayOf(
+        PropTypes.arrayOf(PropTypes.node.isRequired).isRequired
+    ).isRequired,
+
+    // Optional: fixed row height (px).
+    rowHeight: PropTypes.number,
+
+    // Optional: row and column spacing (defaults to 5px).
+    spacing: PropTypes.number,
+
+};
+TreeView.defaultProps = {
+    rowHeight: undefined,
+    spacing: 5,
+};
