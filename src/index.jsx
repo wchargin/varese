86c48f231692defe451be907fe0037be4ec2de69
William Chargin <wchargin@gmail.com>
2015-12-07 19:12:51 -0800

    Create npm package and webpack config boilerplate
    Summary:
    This took way to long to figure out,
    because the Babel 5 to Babel 6 upgrade changed some things
    that I didn't expect to change.
    It seems to be working now, though.
    
    Test Plan:
    Execute `npm start` and visit the page in a web browser.
    See that React loads and a header is rendered.

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+require("babel-polyfill");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var webpack = require('webpack');
+
+var prod = process.env.NODE_ENV === 'production';
+
+function entry() {
+    var mainEntry = './src/index.jsx';
+    if (prod) {
+        return [mainEntry];
+    } else {
+        return [
+            'webpack-dev-server/client?http://localhost:8080',
+            'webpack/hot/only-dev-server',
+            mainEntry,
+        ];
+    }
+}
+
+module.exports = {
+    devtool: prod ? "source-map" : "eval",
+    entry: entry(),
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: (prod ? '' : 'react-hot!') + 'babel?presets[]=react&presets[]=es2015&cacheDirectory',
+            },
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+        ],
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: !prod,
+    },
+    plugins: prod ?
+        [new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+        })] :
+        [new webpack.HotModuleReplacementPlugin()],
+};
