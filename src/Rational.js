338a76de6d386259670e26c211ca1bff931f2039
William Chargin <wchargin@gmail.com>
2015-12-07 20:57:18 -0800

    Extract `Rational` to its own file
    Test Plan:
    Existing unit tests suffice. Run `npm test`.

---
+++ src/Rational.js
@@ -0,0 +1,52 @@
+import {gcd} from './Utils';
+
+export default class Rational {
+
+    constructor(a, b) {
+        if (b === 0) {
+            throw new Error("denominator must not be zero");
+        }
+        if (!isFinite(a)) {
+            throw new Error("numerator must be finite");
+        }
+        if (!isFinite(b)) {
+            throw new Error("denominator must be finite");
+        }
+        if (!isFinite(a / b)) {
+            // can arise when dividing by subnormals, etc.
+            throw new Error("rational must represent a finite quotient");
+        }
+
+        const divisor = gcd(a, b);
+        const maybeNegate = b < 0 ? -1 : 1;
+        const fac = maybeNegate / divisor;
+
+        this.a = a * fac;
+        this.b = b * fac;
+    }
+
+    equals(other) {
+        return this.a === other.a && this.b === other.b;
+    }
+
+    multiply(other) {
+        return new Rational(this.a * other.a, this.b * other.b);
+    }
+
+    exponentiateScalar(k) {
+        if (k === 0) {
+            return new Rational(1, 1);
+        } else {
+            const {a, b} = this;
+            const absK = Math.abs(k);
+            return new Rational(
+                Math.pow(k > 0 ? a : b, absK),
+                Math.pow(k > 0 ? b : a, absK));
+        }
+    }
+
+    toNumber() {
+        return this.a / this.b;
+    }
+
+}
---
+++ test/RationalTest.js
@@ -0,0 +1,97 @@
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+
+import Rational from '../src/Rational';
+
+describe('Rational', () => {
+
+    describe('#constructor', () => {
+        it("creates new Rational objects", () => {
+            const r = new Rational(3, 2);
+            expect(r).to.be.an.instanceof(Rational);
+            expect(r.a).to.equal(3);
+            expect(r.b).to.equal(2);
+        });
+        it("complains when the denominator is zero",
+            () => expect(() => new Rational(1, 0)).to.throw(/zero/));
+        it("reduces the fractions",
+            () => expect(new Rational(8, 6).a).to.equal(4));
+        it("allows negatives in the first term", () => {
+            const r = new Rational(-5, 3);
+            expect(r.a).to.equal(-5);
+            expect(r.b).to.equal(3);
+        });
+        it("normalizes negatives in the second term", () => {
+            const r = new Rational(5, -3);
+            expect(r.a).to.equal(-5);
+            expect(r.b).to.equal(3);
+        });
+
+        it("rejects an infinity in the numerator",
+            () => expect(() => new Rational(Infinity, 1))
+                .to.throw(/numerator/));
+        it("rejects an infinity in the denominator",
+            () => expect(() => new Rational(1, Infinity))
+                .to.throw(/denominator/));
+        it("rejects an infinite quotient of finite numbers", () => {
+            const superSmall = 1e-313;
+            expect(superSmall !== 0).to.be.true;
+            expect(isFinite(superSmall)).to.be.true;
+            expect(() => new Rational(1, superSmall))
+                .to.throw(/finite quotient/);
+        })
+    });
+
+    describe('#equals', () => {
+        it("compares for equality",
+            () => expect(new Rational(8, 6).equals(new Rational(4, 3)))
+                .to.be.true);
+        it("compares for inequality",
+            () => expect(new Rational(8, 6).equals(new Rational(5, 3)))
+                .to.be.false);
+    })
+
+    describe('#multiply', () => {
+        it("multiplies things that would simplify",
+            () => expect(new Rational(8, 6).multiply(new Rational(4, 2))
+                .equals(new Rational(8, 3))).to.be.true);
+        it("multiplies things that would complicate",
+            () => expect(new Rational(8, 6).multiply(new Rational(5, 17))
+                .equals(new Rational(20, 51))).to.be.true);
+        it("multiplies to zero",
+            () => expect(new Rational(17, 11).multiply(new Rational(0, 12))
+                .equals(new Rational(0, 1))).to.be.true);
+    });
+
+    describe('#exponentiateScalar', () => {
+        const exp57 = k => new Rational(5, 7).exponentiateScalar(k);
+        it("exponentiates to the power of 2",
+            () => expect(exp57(2).equals(new Rational(25, 49)))
+                .to.be.true);
+        it("exponentiates to the power of 1 (identity)",
+            () => expect(exp57(1).equals(new Rational(5, 7))).to.be.true);
+        it("exponentiates to the power of 0 (projection)",
+            () => expect(exp57(0).equals(new Rational(1, 1))).to.be.true);
+        it("exponentiates to the power of -1 (reciprocal)",
+            () => expect(exp57(-1).equals(new Rational(7, 5))).to.be.true);
+        it("exponentiates to the power of -2",
+            () => expect(exp57(-2).equals(new Rational(49, 25)))
+                .to.be.true);
+
+        const exp0 = k => new Rational(0, 1).exponentiateScalar(k);
+        it("lets 0^88 = 0",
+            () => expect(exp0(88).equals(new Rational(0, 1))).to.be.true);
+        it("lets 0^0 = 1",
+            () => expect(exp0(0).equals(new Rational(1, 1))).to.be.true);
+    });
+
+    describe("#toNumber", () => {
+        it("converts zero to a number",
+            () => expect(new Rational(0, 12).toNumber()).to.equal(0));
+        it("converts (7 / 1) to a number",
+            () => expect(new Rational(7, 1).toNumber()).to.equal(7));
+        it("converts (7 / 4) to a number",
+            () => expect(new Rational(7, 4).toNumber()).to.equal(7 / 4));
+    });
+
+});
