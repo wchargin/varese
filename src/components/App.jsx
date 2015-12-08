449550daf92fbbc819519ff17281abb058a7fa0b
William Chargin <wchargin@gmail.com>
2015-12-08 08:39:59 -0800

    Add a rationalizer configuration component
    Summary:
    This component provides a bunch of text boxes
    for specifying the acoustic ratios for the simple semitone differences.
    It defaults to the values of the canonical rationalizer.
    
    The editing mode is currently entirely string-based:
    you make changes, then we try to parse those changes with a regex;
    if they work, the result is saved as a Rational.
    (The underlying value is always stored as a Rational, never a string.)
    This seems to actually work out pretty well;
    it's probably going to prove a lot less cumbersome than, say,
    two separate text boxes (one for the numerator, one for denominator).
    The simplification mechanism---namely, you can enter whatever you want
    but as soon as you blur the fraction will be simplified
    (e.g., "4:2" becomes "2:1" when you exit the text field)---
    is quite nice.
    
    Doesn't include nice error handling,
    but I don't think it really needs to:
    you'll know if you divide by zero or something stupid,
    and if you enter something that doesn't match the regex
    you probably just have a typo (`:` to `;`, maybe?)
    and should just try again.
    
    Screenshot:
    ![Screenshot](http://i.imgur.com/VXTV22M.png)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 
+import RationalizerConfig from './RationalizerConfig';
+
 export default class App extends Component {
     render() {
         return <div className="container">
             <h1>Hello, app!</h1>
+            <RationalizerConfig />
         </div>;
     }
 }
---
+++ src/components/RationalizerConfig.jsx
@@ -0,0 +1,138 @@
+import React, {Component, PropTypes} from 'react';
+
+import {canonicalRationalizer} from '../HarmonicSeries';
+import Rational from '../Rational';
+
+export default class RationalizerConfig extends Component {
+    constructor() {
+        super();
+        const initialInputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+        this.state = {
+            values: initialInputs.map(canonicalRationalizer),
+        };
+    }
+    render() {
+        const names = [
+            "m2", "M2", "m3", "M3",
+            "P4", "A4/d5", "P5",
+            "m6", "M6", "m7", "M7"
+        ];
+        return <div>
+            {this.state.values.map((value, index) => {
+                const setValueTo = newValue => {
+                    const {values} = this.state;
+                    this.setState({
+                        values: [
+                        ...values.slice(0, index),
+                        newValue,
+                        ...values.slice(index + 1),
+                        ],
+                    });
+                };
+                const canonicalValue = canonicalRationalizer(index + 1);
+                return <AcousticRatioBox
+                    value={value}
+                    key={index}
+                    name={names[index]}
+                    onChange={newValue => setValueTo(newValue)}
+                    onReset={newValue => setValueTo(canonicalValue)}
+                />;
+            })}
+        </div>;
+    }
+}
+
+class AcousticRatioBox extends Component {
+
+    render() {
+        return <div style={{
+            display: "inline-flex",
+            width: 60,
+            flexDirection: "column",
+            textAlign: "center",
+            marginRight: 5,
+        }}>
+            <strong>{this.props.name}</strong>
+            <AcousticRatioEntry
+                value={this.props.value}
+                onChange={this.props.onChange}
+            />
+            <button onClick={this.props.onReset}>Reset</button>
+        </div>;
+    }
+
+}
+AcousticRatioBox.propTypes = {
+    value: PropTypes.instanceOf(Rational),
+    onChange: PropTypes.func.isRequired,
+
+    onReset: PropTypes.func.isRequired,
+    name: PropTypes.string.isRequired,
+};
+
+/*
+ * State field 'text' represents the text being entered right now;
+ * it should be null when the user is not entering text
+ * (e.g., the field is blurred, or the user hasn't typed anything yet).
+ * If the 'text' state field is present,
+ * it will be used for the value of the input node.
+ *
+ * (This is so that if you type something like "2:4"
+ * it will only be corrected to "1:2" after you exit the box,
+ * so you can actually type something like "2:41.")
+ */
+class AcousticRatioEntry extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            text: null,
+        };
+    }
+
+    render() {
+        const text = this.state.text !== null ?
+            this.state.text :
+            `${this.props.value.a}:${this.props.value.b}`;
+        return <input
+            ref="input"
+            value={text}
+            onChange={() => this._handleChange()}
+            onBlur={() => this.setState({ text: null })}
+            onClick={() => this.refs.input.select()}
+            style={{ width: "100%", textAlign: "center" }}
+        />;
+    }
+
+    _fromString(str) {
+        const match = str.match(/^\s*(\d+)\s*:\s*(\d+)\s*$/);
+        if (!match) {
+            return { status: "error", error: "no match" };
+        } else {
+            const {1: a, 2: b} = match;
+            try {
+                return {
+                    status: "success",
+                    result: new Rational(parseInt(a, 10), parseInt(b, 10)),
+                };
+            } catch (e) {
+                return { status: "error", error: e.message };
+            }
+        }
+    }
+
+    _handleChange() {
+        const text = this.refs.input.value;
+        this.setState({ text });
+
+        const value = this._fromString(text);
+        if (value.result) {
+            this.props.onChange(value.result);
+        }
+    }
+
+}
+AcousticRatioEntry.propTypes = {
+    value: PropTypes.instanceOf(Rational),
+    onChange: PropTypes.func.isRequired,
+};
