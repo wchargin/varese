f0ac8403e6e4e9d0914ecf9bb9afe42b1bdca6df
William Chargin <wchargin@gmail.com>
2015-12-08 09:36:48 -0800

    Rename app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import RationalizerConfig from './RationalizerConfig';
 export default class App extends Component {
     render() {
         return <div className="container">
-            <h1>Hello, app!</h1>
+            <h1>Var&egrave;se pitch calculator</h1>
             <RationalizerConfig />
         </div>;
     }
