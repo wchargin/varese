8c67993dc14285bf0503659d2bd89f3c6d568be6
William Chargin <wchargin@gmail.com>
2015-12-07 20:10:52 -0800

    Implement a Rational class
    Summary:
    This is a bare-bones version of the Data.Ratio module.
    
    Test Plan:
    Unit tests included. Run `npm test`.

--- src/Utils.js
+++ src/Utils.js
@@ -6,6 +6,35 @@ export function gcd(a, b) {
     }
 }
 
+class Rational {
+
+    constructor(a, b) {
+        if (b === 0) {
+            throw new Error("denominator is zero");
+        }
+        const divisor = Math.abs(gcd(a, b));
+        const maybeNegate = b < 0 ? -1 : 1;
+        const fac = maybeNegate / divisor;
+
+        this.a = a * fac;
+        this.b = b * fac;
+    }
+
+    equals(other) {
+        return this.a === other.a && this.b === other.b;
+    }
+
+    multiply(other) {
+        return new Rational(this.a * other.a, this.b * other.b);
+    }
+
+    toNumber() {
+        return this.a / this.b;
+    }
+
+}
+
 export default {
     gcd,
+    Rational,
 };
--- test/UtilsTest.js
+++ test/UtilsTest.js
@@ -24,4 +24,54 @@ describe('Utils', () => {
             () => expect(gcd(24, 17)).to.equal(1));
     });
 
+    describe('.Rational', () => {
+
+        const {Rational} = Utils;
+
+        describe('#constructor', () => {
+            it("creates new Rational objects", () => {
+                const r = new Rational(3, 2);
+                expect(r).to.be.an.instanceof(Rational);
+                expect(r.a).to.equal(3);
+                expect(r.b).to.equal(2);
+            });
+            it("complains when the denominator is zero",
+                () => expect(() => new Rational(1, 0)).to.throw(/zero/));
+            it("reduces the fractions",
+                () => expect(new Rational(8, 6).a).to.equal(4));
+            it("allows negatives in the first term", () => {
+                const r = new Rational(-5, 3);
+                expect(r.a).to.equal(-5);
+                expect(r.b).to.equal(3);
+            });
+            it("normalizes negatives in the second term", () => {
+                const r = new Rational(5, -3);
+                expect(r.a).to.equal(-5);
+                expect(r.b).to.equal(3);
+            });
+        });
+
+        describe('#equals', () => {
+            it("compares for equality",
+                () => expect(new Rational(8, 6).equals(new Rational(4, 3)))
+                    .to.be.true);
+            it("compares for inequality",
+                () => expect(new Rational(8, 6).equals(new Rational(5, 3)))
+                    .to.be.false);
+        })
+
+        describe('#multiply', () => {
+            it("multiplies things that would simplify",
+                () => expect(new Rational(8, 6).multiply(new Rational(4, 2))
+                    .equals(new Rational(8, 3))).to.be.true);
+            it("multiplies things that would complicate",
+                () => expect(new Rational(8, 6).multiply(new Rational(5, 17))
+                    .equals(new Rational(20, 51))).to.be.true);
+            it("multiplies to zero",
+                () => expect(new Rational(17, 11).multiply(new Rational(0, 12))
+                    .equals(new Rational(0, 1))).to.be.true);
+        });
+
+    })
+
 });
