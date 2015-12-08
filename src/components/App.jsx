19f3aa8d4f9ce8a78c940827c873b652b425ea88
William Chargin <wchargin@gmail.com>
2015-12-08 07:44:02 -0800

    Extract app component to own file

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    render() {
+        return <div className="container">
+            <h1>Hello, app!</h1>
+        </div>;
+    }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,6 @@ require("babel-polyfill");
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('app'));
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
