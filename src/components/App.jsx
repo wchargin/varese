11bd8ea094b72c321fd0cadfb0967b9cd8180e7f
William Chargin <wchargin@gmail.com>
2015-12-08 15:47:02 -0800

    Set up routes
    Summary:
    Whoa. That was easy.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
+import {Router, Route, Redirect} from 'react-router';
 
 import PitchCalculator from './PitchCalculator';
 import TreeExplorer from './TreeExplorer';
 
 export default class App extends Component {
     render() {
-        return <div>
-            <PitchCalculator />
-            <TreeExplorer />
-        </div>;
+        return <Router>
+            <Redirect from="/" to="calculator" />
+            <Route path="calculator" component={PitchCalculator} />
+            <Route path="tree" component={TreeExplorer} />
+        </Router>;
     }
 }
