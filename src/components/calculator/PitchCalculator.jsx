3e4769b5c6ce23d15e6ddb189f1132e512f4847f
William Chargin <wchargin@gmail.com>
2015-12-08 17:41:17 -0800

    Allow storage of the rationalizer configuration

--- src/components/calculator/PitchCalculator.jsx
+++ src/components/calculator/PitchCalculator.jsx
@@ -5,6 +5,9 @@ import ChordComputer from './ChordComputer';
 import RationalizerConfig from './RationalizerConfig';
 
 import {extendRationalizer, canonicalRationalizer} from '../../HarmonicSeries';
+import Rational from '../../Rational';
+
+const LOCAL_STORE_KEY = "rationalizer_values";
 const initialInputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 const initialValues = initialInputs.map(canonicalRationalizer);
 const names = [
@@ -13,11 +16,22 @@ const names = [
     "m6", "M6", "m7", "M7"
 ];
 
+/*
+ * Given an array of JSON blobs that were serialized from Rationals,
+ * reconstruct the Rationals.
+ *
+ * Rehydrating things that are already rational is a no-op.
+ */
+function rehydrate(arr) {
+    return arr.map(x => new Rational(x.a, x.b));
+}
+
 export default class PitchCalculator extends Component {
+
     constructor() {
         super();
         this.state = {
-            values: initialValues,
+            values: rehydrate(LocalStore.get(LOCAL_STORE_KEY, initialValues)),
         };
     }
 
@@ -33,13 +47,13 @@ export default class PitchCalculator extends Component {
                 names={names}
                 onChangeValue={(newValue, index) => {
                     const {values} = this.state;
-                    this.setState({
-                        values: [
-                            ...values.slice(0, index),
-                            newValue,
-                            ...values.slice(index + 1),
-                        ],
-                    });
+                    const newValues = [
+                        ...values.slice(0, index),
+                        newValue,
+                        ...values.slice(index + 1),
+                    ];
+                    this.setState({ values: newValues });
+                    LocalStore.set(LOCAL_STORE_KEY, newValues);
                 }}
             />
         </Page>;
