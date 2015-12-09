11149186163faa3f00ea5dadd94a45cc18d4cbd6
William Chargin <wchargin@gmail.com>
2015-12-08 16:57:55 -0800

    Extract the calculator's ChordInput to be reusable

--- src/components/calculator/ChordComputer.jsx
+++ src/components/calculator/ChordComputer.jsx
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 import PitchNames from '../../PitchNames';
 import {findChordRootOffset} from '../../HarmonicSeries';
 
+import ChordInput from '../ChordInput';
+
 const DEFAULT_CHORD = [3, 7, 10];
 
 export default class ChordComputer extends Component {
@@ -37,83 +39,6 @@ export default class ChordComputer extends Component {
         </div>;
     }
 
-}
-
-class ChordInput extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            text: null,
-        };
-    }
-
-    componentDidMount() {
-        const {input} = this.refs;
-        input.focus();
-        input.selectionStart = input.value.length;
-        input.selectionEnd = input.value.length;
-    }
-
-    render() {
-        const text = this.state.text !== null ?
-            this.state.text :
-            this._toString(this.props.value);
-        return <div className="well">
-            <strong>Enter a chord:</strong>
-            {" "}
-            <input
-                className="form-control"
-                ref="input"
-                value={text}
-                onChange={() => this._handleChange()}
-                onBlur={() => this.setState({ text: null })}
-                style={{
-                    color: this._fromString(text).status === "error" ?
-                        "red" :
-                        undefined,
-                    display: "inline-block",
-                    width: "unset",
-                }}
-            />
-        </div>;
-    }
-
-    _handleChange() {
-        const text = this.refs.input.value;
-        this.setState({ text });
-
-        const value = this._fromString(text);
-        if (value.result) {
-            this.props.onChange(value.result);
-        }
-    }
-
-    _toString(value) {
-        return value.map(x => PitchNames.pitchToName(x, true)).join(", ");
-    }
-
-    _fromString(str) {
-        const trimmed = str
-            .replace(/\s/g, "")     // internal spaces
-            .replace(/^,*/, "")     // leading commas
-            .replace(/,,+/g, ",")   // internal commas
-            .replace(/,+$/, "");    // trailing commas
-        if (trimmed.length === 0) {
-            return { status: "success", result: [] };
-        }
-
-        const parts = trimmed.split(",").map(PitchNames.parseNameOrPitch);
-        if (parts.filter(x => x === null).length !== 0) {
-            return { status: "error", error: "some parts of parse failed" };
-        }
-        parts.sort((a, b) => a - b);
-
-        return {
-            status: "success",
-            result: parts,
-        };
-    }
 }
 
 class ChordOutput extends Component {
