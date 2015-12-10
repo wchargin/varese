68728049172d2766772a20cbeac4d514ef906261
William Chargin <wchargin@gmail.com>
2015-12-10 13:04:36 -0800

    Add a canonical infolding function
    Test Plan:
    Unit tests included. Run `npm test`.

--- src/Folding.js
+++ src/Folding.js
@@ -8,7 +8,24 @@ export function outfoldDown(arr) {
     return [low - (mid - low), low, high];
 }
 
+export function infoldCanonical(arr) {
+    const [low, mid, high] = arr;
+    const [deltaLow, deltaHigh] = [mid - low, high - mid];
+
+    if (deltaLow < deltaHigh) {
+        // Infold up.
+        return [mid, mid + deltaLow, high];
+    } else if (deltaHigh < deltaLow) {
+        // Infold down.
+        return [low, mid - deltaHigh, mid];
+    } else {
+        // This is the root of the tree; there is no infolding.
+        return arr;
+    }
+}
+
 export default {
     outfoldUp,
     outfoldDown,
+    infoldCanonical,
 };
--- test/FoldingTest.js
+++ test/FoldingTest.js
@@ -27,4 +27,20 @@ describe('Folding', () => {
             test([-4, 0, 7], [-8, -4, 7]));
     });
 
+    describe('#infoldCanonical', () => {
+        const {infoldCanonical} = Folding;
+        const test = (input, output) =>
+            () => expect(infoldCanonical(input)).to.deep.equal(output);
+
+        it("folds [0, 4, 7] down to [0, 1, 4]",
+            test([0, 4, 7], [0, 1, 4]));
+        it("folds [0, 3, 7] up to [3, 6, 7]",
+            test([0, 3, 7], [3, 6, 7]));
+
+        it("folds [5, 9, 16] up to [9, 13, 16]",
+            test([5, 9, 16], [9, 13, 16]));
+        it("folds [5, 12, 16] down to [5, 8, 12]",
+            test([5, 12, 16], [5, 8, 12]));
+    });
+
 });
