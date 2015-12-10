b0173e1180f11d483d0c1047cd641089ef77f60a
William Chargin <wchargin@gmail.com>
2015-12-10 12:23:06 -0800

    Add a note about hover-chords

--- src/components/tree/TreeExplorer.jsx
+++ src/components/tree/TreeExplorer.jsx
@@ -28,6 +28,17 @@ export default class TreeExplorer extends Component {
             Click on any chord to set it as the root,
             or enter a root chord manually in the text box below.
             </p>
+            <div className="alert alert-info">
+                <p>
+                <strong>Experimental:</strong>
+                {" "}
+                Hover over a chord to see it engraved on a staff!
+                This should work for the most part,
+                but it has some issues when the chord is really tall
+                or entirely above or below the staff.
+                If this bothers you, let me know and I'll try to fix it.
+                </p>
+            </div>
             <ChordInput
                 value={this.state.rootChord}
                 message="Enter a trichord: "
