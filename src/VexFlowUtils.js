8f50a35c202ef72a301546c31c71c969013cc5a8
William Chargin <wchargin@gmail.com>
2015-12-10 11:35:57 -0800

    Add a VexFlowUtils file to generate notes
    Test Plan:
    Unit tests included. Run `npm test`.

---
+++ src/VexFlowUtils.js
@@ -0,0 +1,65 @@
+import Vex from 'vexflow';
+
+const baseTable = [
+    { note: "C", accidental: null },
+    { note: "C", accidental: "#" },
+    { note: "D", accidental: null },
+    { note: "E", accidental: "b" },
+    { note: "E", accidental: null },
+    { note: "F", accidental: null },
+    { note: "F", accidental: "#" },
+    { note: "G", accidental: null },
+    { note: "A", accidental: "b" },
+    { note: "A", accidental: null },
+    { note: "B", accidental: "b" },
+    { note: "B", accidental: null },
+];
+
+/*
+ * Input: an array containing numbers of semitones above middle C
+ * (e.g., [0, 4, 7] for a middle C major triad).
+ *
+ * Output: a Vex.Flow.StaveNote.
+ *
+ * Optional parameter: duration, in VexFlow notation (default = "w").
+ */
+export function pitchesToStaveNote(pitches, duration = "w") {
+    const {StaveNote, Accidental} = Vex.Flow;
+
+    // Put the data in a more structured form.
+    const keysData = pitches.map(semitones => {
+        const octaveOffset = Math.floor(semitones / 12);
+        const octaveName = octaveOffset + 4;
+
+        const prePhase = semitones % 12;
+        const phase = prePhase < 0 ? prePhase + 12 : prePhase;
+
+        return {
+            ...baseTable[phase],
+            octave: octaveName,
+        };
+    });
+
+    // Generate the key description text that VexFlow expects.
+    const keys = keysData.map(keyData => {
+        const {note, octave} = keyData;
+        const accidental = keyData.accidental || "";
+        return `${note.toLowerCase()}${accidental}/${octave}`;
+    });
+    const bareNote = new StaveNote({ keys, duration });
+
+    // VexFlow makes us add accidentals in two places:
+    // in the 'keys', as above (e.g., "c#/4"),
+    // and additionally to the generated StaveNote.
+    const noteWithAccidentals = keysData.reduce((acc, keyData, index) =>
+        keyData.accidental ?
+            acc.addAccidental(index, new Accidental(keyData.accidental)) :
+            acc,
+        bareNote);
+
+    return noteWithAccidentals;
+}
+
+export default {
+    pitchesToStaveNote,
+};
---
+++ test/VexFlowUtilsTest.js
@@ -0,0 +1,49 @@
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+
+import VexFlowUtils from '../src/VexFlowUtils';
+
+describe('VexFlowUtils', () => {
+
+    describe('#pitchesToStaveNote', () => {
+        const {pitchesToStaveNote} = VexFlowUtils;
+
+        // Vex.Flow.StaveNote#getAccidentals is broken.
+        // We can work around that.
+        // At the same time, we extract just the useful data.
+        function getAccidentals(staveNote) {
+            return staveNote.modifiers
+                .filter(modifier => modifier.getCategory() === "accidentals")
+                .map(modifier => ({
+                index: modifier.index,
+                type: modifier.type,
+            }));
+        }
+
+        const makeCTriad = () => pitchesToStaveNote([0, 4, 7]);
+        it("generates the correct notes for a middle C major triad", () =>
+            expect(makeCTriad().getKeys()).to.deep.equal(
+                ["c/4", "e/4", "g/4"]));
+        it("adds no accidentals to a middle C major triad", () =>
+            expect(getAccidentals(makeCTriad())).to.deep.equal([]));
+
+        // Really we'd like this to be C/E/G#,
+        // but it's going to come out as C/E/Ab.
+        // That's fine; deal with it.
+        const makeCAug = () => pitchesToStaveNote([0, 4, 8]);
+        it("generates the correct notes for a middle C augmented triad", () =>
+            expect(makeCAug().getKeys()).to.deep.equal(
+                ["c/4", "e/4", "ab/4"]));
+        it("adds the correct accidentals to a middle C augmented triad", () =>
+            expect(getAccidentals(makeCAug())).to.deep.equal(
+                [{ index: 2, type: "b" }]));
+
+        const makeWide = () => pitchesToStaveNote([-12, 0, 16]);
+        it("generates the correct pitches for an open compound M3", () =>
+            expect(makeWide().getKeys()).to.deep.equal(
+                ["c/3", "c/4", "e/5"]));
+        it("adds no accidentals to this particular chord", () =>
+            expect(getAccidentals(makeWide())).to.deep.equal([]));
+    });
+
+});
