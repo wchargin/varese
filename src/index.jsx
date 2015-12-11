6ce3c881f4a4b29c0c226f4a92abf457244d5a77
William Chargin <wchargin@gmail.com>
2015-12-11 15:12:35 -0800

    Add the Redux store to the application
    Summary:
    It doesn't do anything yet.

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,15 @@ require("babel-polyfill");
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import Reducer from './Reducers';
+
 import App from './components/App';
 require("./LocalStore");
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const store = createStore(Reducer);
+const component = <Provider store={store}>
+    <App />
+</Provider>;
+ReactDOM.render(component, document.getElementById('app'));
