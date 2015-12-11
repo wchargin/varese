aea1f7ca4a6d334f4637b1c1cc0c6fe67c4dc28e
William Chargin <wchargin@gmail.com>
2015-12-10 20:12:46 -0800

    Add an `arraysEqual` helper function
    Summary:
    …and have fun with fixed points at the same time!
    
    Test Plan:
    Unit tests included. Run `npm test`.

--- src/Utils.js
+++ src/Utils.js
@@ -6,6 +6,19 @@ export function gcd(a, b) {
     }
 }
 
+export function arraysEqual(arr1, arr2, comparator = (a, b) => a === b) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        if (!comparator(arr1[i], arr2[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
 export default {
     gcd,
+    arraysEqual,
 };
--- test/UtilsTest.js
+++ test/UtilsTest.js
@@ -38,4 +38,58 @@ describe('Utils', () => {
             () => expect(gcd(-5, -15)).to.equal(5));
     });
 
+    describe('#arraysEqual', () => {
+        const {arraysEqual} = Utils;
+
+        const arr1 = [1, 2, 3];
+        it("works for identical arrays", () =>
+            expect(arraysEqual(arr1, arr1)).to.be.true);
+        it("works for non-identical but equal arrays", () =>
+            expect(arraysEqual(arr1, [1, 2, 3])).to.be.true);
+
+        it("fails when the first array is longer", () =>
+            expect(arraysEqual(arr1, [1, 2])).to.be.false);
+        it("fails when the first array is shorter", () =>
+            expect(arraysEqual(arr1, [1, 2, 3, 4])).to.be.false);
+        it("fails when the first element differs", () =>
+            expect(arraysEqual(arr1, [8, 2, 3])).to.be.false);
+        it("fails when the middle element differs", () =>
+            expect(arraysEqual(arr1, [1, 8, 3])).to.be.false);
+        it("fails when the last element differs", () =>
+            expect(arraysEqual(arr1, [1, 2, 8])).to.be.false);
+
+        const neq = (a, b) => a !== b;
+        it("works with a non-standard comparator in the positive case", () =>
+            expect(arraysEqual(arr1, [2, 3, 4], neq)).to.be.true);
+        it("works with a non-standard comparator in the negative case", () =>
+            expect(arraysEqual(arr1, [3, 2, 1], neq)).to.be.false);
+
+        const nest = (a, b) => arraysEqual(a, b);
+        it("works with itself as the comparator in the positive case", () =>
+            expect(arraysEqual([[1, 2], [3, 4]], [[1, 2], [3, 4]], nest))
+                .to.be.true);
+        it("works with itself as the comparator in the negative case", () =>
+            expect(arraysEqual([[1, 2], [3, 4]], [[1, 2], [3, 8]], nest))
+                .to.be.false);
+
+        // This one's more just for fun...
+        const improve = f => (a, b) => {
+            const [arrA, arrB] = [a, b].map(arr => Array.isArray(arr));
+            if (arrA && arrB) {
+                return arraysEqual(a, b, f);
+            } else if (!arrA && !arrB) {
+                return a === b;
+            } else {
+                return false;
+            }
+        };
+        const fix = (f => f(f))(f => improve((a, b) => f(f)(a, b)));
+        it("works with the fixed point of itself in the positive case", () =>
+            expect(arraysEqual([1, [2, [3, [4]]]], [1, [2, [3, [4]]]], fix))
+                .to.be.true);
+        it("works with the fixed point of itself in the negative case", () =>
+            expect(arraysEqual([1, [2, [3, [4]]]], [1, [2, [8, [4]]]], fix))
+                .to.be.false);
+    });
+
 });
