0097b42b87feec5b99b56577c355114f33438739
William Chargin <wchargin@gmail.com>
2015-12-10 20:51:35 -0800

    Remove the "experimental" warning about engravings
    Summary:
    See f390e4a for details.

--- src/components/tree/TreeExplorer.jsx
+++ src/components/tree/TreeExplorer.jsx
@@ -25,20 +25,13 @@ export default class TreeExplorer extends Component {
             and branches to the right are outfoldings up.
             </p>
             <p>
-            Click on any chord to set it as the root,
-            or enter a root chord manually in the text box below.
+            <strong>Click</strong> on any chord to set it as the root,
+            or <strong>type</strong> a root chord manually
+            in the text box below.
             </p>
-            <div className="alert alert-info">
-                <p>
-                <strong>Experimental:</strong>
-                {" "}
-                Hover over a chord to see it engraved on a staff!
-                This should work for the most part,
-                but it has some issues when the chord is really tall
-                or entirely above or below the staff.
-                If this bothers you, let me know and I'll try to fix it.
-                </p>
-            </div>
+            <p>
+            <strong>Hover</strong> over a chord to see it engraved on a staff!
+            </p>
             <ChordInput
                 value={this.state.rootChord}
                 message="Enter a trichord: "
