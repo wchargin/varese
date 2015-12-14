a6d4e22bba6fe4b9a2bda0ee6769bfe9e5db7683
William Chargin <wchargin@gmail.com>
2015-12-13 22:30:41 -0800

    Make sourcemaps a bit better
    Summary:
    By adding `retainLines`, we now have only a constant offset
    due to two hot-loader lines I can't seem to get rid of.

--- webpack.config.js
+++ webpack.config.js
@@ -15,22 +15,53 @@ function entry() {
     }
 }
 
+function loaders() {
+    var base = [
+        !prod && {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'react-hot',
+        },
+        {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['react', 'es2015'],
+                plugins: 'transform-object-rest-spread',
+                retainLines: true,
+                cacheDirectory: true,
+            },
+        },
+        {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader',
+        },
+        {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "file",
+        },
+        {
+            test: /\.(woff|woff2)$/,
+            loader:"url?prefix=font/&limit=5000",
+        },
+        {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url?limit=10000&mimetype=application/octet-stream",
+        },
+        {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url?limit=10000&mimetype=image/svg+xml",
+        },
+    ];
+    return base.filter(function(x) { return x; });
+}
+
 module.exports = {
     devtool: prod ? "source-map" : "eval",
     entry: entry(),
     module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: (prod ? '' : 'react-hot!') + 'babel?presets[]=react&presets[]=es2015&plugins[]=transform-object-rest-spread&cacheDirectory',
-            },
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-            { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
-            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
-        ],
+        loaders: loaders(),
     },
     resolve: {
         extensions: ['', '.js', '.jsx'],
