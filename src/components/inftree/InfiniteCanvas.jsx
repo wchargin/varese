25114d2d1bf462c8288d9e82eba26f5dce00d2c7
William Chargin <wchargin@gmail.com>
2015-12-16 13:37:18 -0800

    Make canvas responsive

--- src/components/inftree/InfiniteCanvas.jsx
+++ src/components/inftree/InfiniteCanvas.jsx
@@ -3,7 +3,12 @@ import React, {Component} from 'react';
 export default class InfiniteCanvas extends Component {
 
     render() {
-        return <canvas ref="canvas">
+        return <canvas
+            ref="canvas"
+            width={720}
+            height={600}
+            style={{ outline: "thin red solid", width: "100%", height: 600 }}
+        >
             <div className="alert alert-danger">
                 <strong>Uh oh!</strong>
                 {" "}
@@ -15,18 +20,48 @@ export default class InfiniteCanvas extends Component {
         </canvas>;
     }
 
-    componentDidMount() {
+    _draw() {
         const {canvas} = this.refs;
         const ctx = canvas.getContext('2d');
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        ctx.fillRect(0, Math.random() * 100, 75, 3);
+        ctx.fillRect(0, 16, 75, 2);
         ctx.font = "12px sans";
         ctx.fillText("Hello, world!", 0, 14);
     }
 
+    _resizeCanvas() {
+        const {canvas} = this.refs;
+        const widthString = window.getComputedStyle(canvas).width;
+        const width = parseInt(widthString.match(/(\d+)px/)[1], 10);
+        if (width !== canvas.width) {
+            canvas.width = width;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    componentDidMount() {
+        this._resizeCanvas();
+        this._draw();
+
+        this._resizeListener = () => {
+            if (this._resizeCanvas()) {
+                console.log("New size!");
+                this._draw();
+            }
+        };
+        window.addEventListener('resize', this._resizeListener);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this._resizeListener);
+    }
+
     componentDidUpdate() {
-        this.componentDidMount();
+        this._resizeCanvas();
+        this._draw();
     }
 
 }
