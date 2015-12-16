cd5f3a90353329888db1dd98729faa58361388b4
William Chargin <wchargin@gmail.com>
2015-12-16 12:31:48 -0800

    Add an infinite tree placeholder

---
+++ src/components/inftree/InfiniteTreeExplorer.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+import Page from '../Page';
+
+export default class InfiniteTreeExplorer extends Component {
+    render() {
+        return <Page path="infinite-tree">
+            <h1>Infinite chord tree explorer (alpha)</h1>
+        </Page>;
+    }
+}
