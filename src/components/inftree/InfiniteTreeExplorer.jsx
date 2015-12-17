ef4d12c6e091e060682261ffc02b5a5804457afe
William Chargin <wchargin@gmail.com>
2015-12-17 11:56:48 -0800

    Add ViewOptions to the infinite tree
    Summary:
    For now, these share the same bindings as the finite tree.
    We'll see whether that's a good idea.

--- src/components/inftree/InfiniteTreeExplorer.jsx
+++ src/components/inftree/InfiniteTreeExplorer.jsx
@@ -1,14 +1,65 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import * as Actions from '../../Actions';
 
 import Page from '../Page';
 
 import InfiniteCanvas from './InfiniteCanvas';
+import ViewOptions from '../tree/ViewOptions';
 
-export default class InfiniteTreeExplorer extends Component {
+class InfiniteTreeExplorer extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            rootChord: [0, 4, 7],
+        };
+    }
+
+    /* eslint-disable react/prop-types */
     render() {
         return <Page path="infinite-tree">
             <h1>Infinite chord tree explorer (alpha)</h1>
-            <InfiniteCanvas />
+            <ViewOptions
+                {...this.props.treeViewOptions}
+                {...this.props.viewOptionsHandlers}
+            />
+            <InfiniteCanvas
+                levels={this.props.treeViewOptions.levels}
+            />
         </Page>;
     }
+    /* eslint-enable */
+
 }
+
+function mapStateToProps(state) {
+    return {
+        acousticRatios: state.acousticRatios,
+        rootsVisible: state.treeViewOptions.showRoots,
+        treeViewOptions: state.treeViewOptions,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        onSetAcousticRatio: (index, ratio) => dispatch(
+            Actions.setAcousticRatio(index, ratio)),
+        viewOptionsHandlers: {
+            onSetLevels: levels => dispatch(Actions.setTreeLevels(levels)),
+            onSetShowRoots: showRoots => dispatch(
+                Actions.setTreeShowRoots(showRoots)),
+            onSetShowOctaves: showOctaves => dispatch(
+                Actions.setTreeShowOctaves(showOctaves)),
+            onSetWide: wide => dispatch(Actions.setTreeWide(wide)),
+            onSetLimitValue: (limit, value) => dispatch(
+                Actions.setTreeLimitValue(limit, value)),
+            onSetLimitEnabled: (limit, enabled) => dispatch(
+                Actions.setTreeLimitEnabled(limit, enabled)),
+        },
+    };
+}
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+export default connector(InfiniteTreeExplorer);
