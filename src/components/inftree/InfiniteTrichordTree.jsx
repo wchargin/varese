822785eca5183a252d1e8e2064804c1b419ea51a
William Chargin <wchargin@gmail.com>
2015-12-17 12:03:06 -0800

    Implement a wide view of the infinite canvas

--- src/components/inftree/InfiniteTrichordTree.jsx
+++ src/components/inftree/InfiniteTrichordTree.jsx
@@ -5,7 +5,14 @@ import InfiniteCanvas from './InfiniteCanvas';
 export default class InfiniteTrichordTree extends Component {
 
     render() {
-        return <div>
+        const widePadding = 20;
+        const wideStyle = this.props.viewOptions.wide ? {
+            position: "relative",
+            width: `calc(100vw - ${2 * widePadding}px)`,
+            left: `calc(-50vw + ${widePadding}px + 50%)`,
+        } : {};
+
+        return <div style={{...wideStyle, marginBottom: 20}}>
             <InfiniteCanvas
                 levels={this.props.viewOptions.levels}
             />
