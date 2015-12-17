3e9fb0b639756d7c56273b28c4001dce2763986e
William Chargin <wchargin@gmail.com>
2015-12-17 11:37:25 -0800

    Trash the old infinite canvas attempt

--- src/components/inftree/InfiniteTreeExplorer.jsx
+++ src/components/inftree/InfiniteTreeExplorer.jsx
@@ -2,13 +2,13 @@ import React, {Component} from 'react';
 
 import Page from '../Page';
 
-import NewInfiniteCanvas from './NewInfiniteCanvas';
+import InfiniteCanvas from './InfiniteCanvas';
 
 export default class InfiniteTreeExplorer extends Component {
     render() {
         return <Page path="infinite-tree">
             <h1>Infinite chord tree explorer (alpha)</h1>
-            <NewInfiniteCanvas />
+            <InfiniteCanvas />
         </Page>;
     }
 }
