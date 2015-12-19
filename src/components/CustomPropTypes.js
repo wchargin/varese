a389c8c264442413cf84c3a8c5aa2f10bda0d9ae
William Chargin <wchargin@gmail.com>
2015-12-19 11:20:49 -0800

    Don't export raw shape propTypes

--- src/components/CustomPropTypes.js
+++ src/components/CustomPropTypes.js
@@ -1,6 +1,6 @@
 import {PropTypes} from 'react';
 
-export const limitsShape = {
+export const limits = PropTypes.shape({
     minCombined: PropTypes.number.isRequired,
     maxCombined: PropTypes.number.isRequired,
     minIndividual: PropTypes.number.isRequired,
@@ -9,42 +9,35 @@ export const limitsShape = {
     maxCombinedEnabled: PropTypes.bool.isRequired,
     minIndividualEnabled: PropTypes.bool.isRequired,
     maxIndividualEnabled: PropTypes.bool.isRequired,
-};
-export const limits = PropTypes.shape(limitsShape);
+});
 
-export const limitsHandlersShape = {
+const limitsHandlersShape = {
     onSetLimitValue: PropTypes.func.isRequired,
     onSetLimitEnabled: PropTypes.func.isRequired,
 };
 export const limitsHandlers = PropTypes.shape(limitsHandlersShape);
 
-export const viewOptionsShape = {
+export const viewOptions = PropTypes.shape({
     levels: PropTypes.number.isRequired,
     infiniteLevels: PropTypes.number.isRequired,
     showRoots: PropTypes.bool.isRequired,
     showOctaves: PropTypes.bool.isRequired,
     wide: PropTypes.bool.isRequired,
     limits: limits.isRequired,
-};
-export const viewOptions = PropTypes.shape(viewOptionsShape);
+});
 
-export const viewOptionsHandlersShape = {
+export const viewOptionsHandlers = PropTypes.shape({
     onSetInfiniteLevels: PropTypes.func.isRequired,
     onSetLevels: PropTypes.func.isRequired,
     onSetShowRoots: PropTypes.func.isRequired,
     onSetShowOctaves: PropTypes.func.isRequired,
     onSetWide: PropTypes.func.isRequired,
     ...limitsHandlersShape,
-};
-export const viewOptionsHandlers = PropTypes.shape(viewOptionsHandlersShape);
+});
 
 export default {
-    limitsShape,
     limits,
-    limitsHandlersShape,
     limitsHandlers,
-    viewOptionsShape,
     viewOptions,
-    viewOptionsHandlersShape,
     viewOptionsHandlers,
 };
