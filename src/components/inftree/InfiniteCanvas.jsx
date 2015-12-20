c2f760fcdbcf4cc6be26ce4737f723d110029d51
William Chargin <wchargin@gmail.com>
2015-12-19 17:06:24 -0800

    Remove border to make fonts smoother
    Summary:
    The border was pushing everything down by half a pixel,
    making things (almost) never align on pixel boundaries,
    and thus making everything look fuzzy.
    By removing the border, we prevent this, and it looks better anyway!

--- src/components/inftree/InfiniteCanvas.jsx
+++ src/components/inftree/InfiniteCanvas.jsx
@@ -144,7 +144,6 @@ export default class InfiniteCanvas extends Component {
             width={720}
             height={this.props.height}
             style={{
-                border: 'thin solid rgba(0, 0, 0, 0.5)',
                 boxShadow: '0 0 2px 1px rgba(0, 0, 0, 0.2)',
                 borderRadius: 2,
                 width: "100%",
