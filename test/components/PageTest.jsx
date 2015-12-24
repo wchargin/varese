e5faa1c4eb1f94bd84f613b30023dde2b25f940c
William Chargin <wchargin@gmail.com>
2015-12-23 17:20:05 -0800

    Improve Page test to test more things
    Summary:
    We now test that the flags are rendered properly.
    In doing so, we use a more robust mechanism than before,
    which is now possible due to the dynamic list of links.
    
    Test Plan:
    Run `npm test`.

--- test/components/PageTest.jsx
+++ test/components/PageTest.jsx
@@ -12,7 +12,12 @@ import Page from '../../src/components/Page';
 
 describe('Page', () => {
 
-    const component = <Page path="tree">
+    const links = [
+        { path: "alpha", name: "The first"  },
+        { path: "beta",  name: "The second" },
+        { path: "gamma", name: "The third", flag: "radiation danger" },
+    ];
+    const component = <Page path="beta" links={links}>
         <div>
             <h1>Content!</h1>
             <span className="amazing-stuff">Inspiring!</span>
@@ -28,21 +33,44 @@ describe('Page', () => {
         expect(spans[0].textContent).to.equal('Inspiring!');
     });
 
+    it("renders all types of links properly", () => {
+        const component = render();
+        const links = scryManyWithTag(component, 'li');
+        expect(links).to.have.length(3);
+
+        // Inactive link.
+        expect(links[0].textContent).to.equal("The first");
+
+        // Active link.
+        expect(links[1].textContent).to.equal("The second");
+
+        // Link with a flag. 'gammaLink' takes on the following shape:
+        // <li><a><span>Name</span><span red>[thicksp]FLAG</span></a></li>
+        // (The <a> tag comes from React Router.)
+        const gammaLink = links[2];
+        const gammaSpans = gammaLink.childNodes[0].childNodes;
+        expect(gammaSpans).to.have.length(2);
+        expect(gammaSpans[0].textContent).to.equal("The third");
+        expect(gammaSpans[1].textContent).to.equal("\u2002RADIATION DANGER");
+        expect(gammaSpans[1].style.color).to.equal("red");
+    });
+
     it("marks the current page as active", () => {
         const component = render();
         const activeLinks = scryRenderedDOMComponentsWithClass(
             component, 'active');
         expect(activeLinks).to.have.length(1);
-        expect(activeLinks[0].textContent).to.equal('Tree explorer');
+        expect(activeLinks[0].textContent).to.equal("The second");
     });
 
     it("marks other pages as inactive", () => {
         const component = render();
-        const inactiveLinks = scryManyWithTag(component, 'li')
-            .filter(x => !x.className.match(/\bactive\b/));
-        expect(inactiveLinks).to.have.length.at.least(1);
-        expect(inactiveLinks.find(x => x.textContent === 'Root calculator'))
-            .to.not.equal(undefined);
+        const inactiveContent = scryManyWithTag(component, 'li')
+            .filter(x => !x.className.match(/\bactive\b/))
+            .map(x => x.textContent);
+        expect(inactiveContent).to.have.length(2);
+        expect(inactiveContent[0]).to.equal("The first");
+        expect(inactiveContent[1]).to.contain("The third");
     });
 
 });
