5ef9b3148c6e990f9636a9846d806f025222a997
William Chargin <wchargin@gmail.com>
2015-12-26 13:54:36 -0800

    Add a Git hooks setup script
    Summary:
    Currently runs the linter.

---
+++ setup_git_hooks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Idempotent script to set up '.git/hooks' files.
+
+set -o errexit
+
+cd "$(dirname "$(readlink -f "$0")")"
+
+HOOKS_DIR="./.git/hooks"
+mkdir -p "$HOOKS_DIR"
+
+PRE_COMMIT_FILE="$HOOKS_DIR/pre-commit"
+PRE_COMMIT_TEXT='npm run lint'
+if ! grep -x "$PRE_COMMIT_TEXT" "$PRE_COMMIT_FILE" 1>/dev/null 2>&1; then
+    echo "$PRE_COMMIT_TEXT" >> "$PRE_COMMIT_FILE" 
+    echo 'Installed pre-commit hook.'
+else
+    echo 'Pre-commit hook already installed.'
+fi
+
+echo 'Done.'
