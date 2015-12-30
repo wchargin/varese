e193cffcd393810bdb4e431e89ff7fc52a21f262
William Chargin <wchargin@gmail.com>
2015-12-30 01:39:38 -0800

    Require unit tests in the commit hook

--- setup_git_hooks.sh
+++ setup_git_hooks.sh
@@ -9,7 +9,7 @@ HOOKS_DIR="./.git/hooks"
 mkdir -p "$HOOKS_DIR"
 
 PRE_COMMIT_FILE="$HOOKS_DIR/pre-commit"
-PRE_COMMIT_TEXT='npm run lint'
+PRE_COMMIT_TEXT='npm run test'
 if ! grep -x "$PRE_COMMIT_TEXT" "$PRE_COMMIT_FILE" 1>/dev/null 2>&1; then
     echo "$PRE_COMMIT_TEXT" >> "$PRE_COMMIT_FILE" 
     echo 'Installed pre-commit hook.'
